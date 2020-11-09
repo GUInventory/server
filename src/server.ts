4beedeecea8f4f0b5068b572c0e739b14a8b2e23
balintkiraly <balint.kiraly@protonmail.com>
2020-11-09 18:48:02 +0100

    Init websocket

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from 'apollo-server-express'
 import { createServer } from 'http'
 import { PrismaClient } from '@prisma/client'
 import { applyMiddleware } from 'graphql-middleware'
+import { PubSub } from 'apollo-server'
 import express from 'express'
 import { schema } from './schema'
 import { shield } from './shield'
@@ -11,14 +12,18 @@ const PORT = 4000
 const app = express()
 const server = createServer(app)
 const prisma = new PrismaClient()
+const pubsub = new PubSub()
 
 const apollo = new ApolloServer({
-  context: ({ req }) => ({ req, prisma }),
+  context: ({ req }) => ({ req, prisma, pubsub }),
   schema: applyMiddleware(schema, shield),
 })
 
 apollo.applyMiddleware({ app, cors: true })
 
-server.listen({ port: PORT }, () => {
-  process.stdout.write(`🚀 Server running on :${PORT}\n`)
+apollo.installSubscriptionHandlers(server)
+
+server.listen(PORT, () => {
+  console.log(`🚀 Server ready at http://localhost:${PORT}${apollo.graphqlPath}`)
+  console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${apollo.subscriptionsPath}`)
 })
