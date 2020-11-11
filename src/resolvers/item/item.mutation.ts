0d83f9df3f97918c9c89585fd9dfe312fc9b9ed6
balintkiraly <balint.kiraly@protonmail.com>
2020-11-11 19:37:08 +0100

    Add itemDelete resolver

--- src/resolvers/item/item.mutation.ts
+++ src/resolvers/item/item.mutation.ts
@@ -1,4 +1,4 @@
-import { extendType } from '@nexus/schema'
+import { extendType, intArg } from '@nexus/schema'
 import { CreateItemInput } from './item.input'
 
 export const ItemMutation = extendType({
@@ -9,6 +9,18 @@ export const ItemMutation = extendType({
       args: { data: CreateItemInput.asArg({ required: true }) },
       resolve: (_, { data }, ctx) => {
         return ctx.prisma.item.create(data)
+      },
+    })
+
+    t.field('deleteItem', {
+      type: 'Item',
+      args: {
+        id: intArg({ required: true }),
+      },
+      resolve: (_, { id }, ctx) => {
+        return ctx.prisma.item.delete({
+          where: { id },
+        })
       },
     })
   },
